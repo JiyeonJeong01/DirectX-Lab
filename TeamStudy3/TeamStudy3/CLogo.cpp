77323462db896d883352f34595965a29fae56689
Trevorjun <150532881+Trevorjun@users.noreply.github.com>
2025-10-19 15:34:15 +0900

    chore : temp

--- TeamStudy3/TeamStudy3/CLogo.cpp
+++ TeamStudy3/TeamStudy3/CLogo.cpp
@@ -26,8 +26,10 @@ void CLogo::Late_Update()
 
 void CLogo::Render(HDC _hDC)
 {
-    HDC	hGroundDC = CBmpManager::Get_Instance()->Find_Img(L"Logo");
-    BitBlt(_hDC, 0, 0, WINCX, WINCY, hGroundDC, 0, 0, SRCCOPY);
+    //HDC	hGroundDC = CBmpManager::Get_Instance()->Find_Img(L"Logo");
+    //BitBlt(_hDC, 0, 0, WINCX, WINCY, hGroundDC, 0, 0, SRCCOPY);
+
+    Rectangle(_hDC, -10, -10, WINCX + 10, WINCY + 10);
 }
 
 void CLogo::Release()
