3abc6d1555c1962ab6a653bc3a7be97e11aae418
InsuMoon2 <soo93689314@gmail.com>
2025-10-19 15:10:56 +0900

    Revert "Merge branch 'feat/Stage03' into develop"
    This reverts commit 5a495c9ed3f703952353d69620d14149c142fb12, reversing
    changes made to 9dfb23a8a86d44da0fa33ec37af69fd7d7f260ea.

--- TeamStudy3/TeamStudy3/CObjectManager.cpp
+++ TeamStudy3/TeamStudy3/CObjectManager.cpp
@@ -1,8 +1,9 @@
 ﻿#include "pch.h"
 #include "CObjectManager.h"
-
+#include "CAbstractFactory.h"
 #include "CCollisionManager.h"
 #include "CObject.h"
+#include "CPlatform.h"
 
 CObjectManager* CObjectManager::m_pInstance = nullptr;
 
@@ -60,7 +61,6 @@ void CObjectManager::Late_Update()
 			obj->Late_Update();
 
     CCollisionManager::Collision_Circle(m_ObjectList[BULLET], m_ObjectList[MONSTER]);
-    CCollisionManager::Collision_Circle(m_ObjectList[ITEM], m_ObjectList[PLAYER]);
     CCollisionManager::Stage01_Collision_Rect(m_ObjectList[PLAYER], m_ObjectList[BLOCK]);
 }
 
@@ -83,4 +83,11 @@ void CObjectManager::Release()
 				}
 			});
 
+}
+
+void CObjectManager::Add_Platform(Vec3 _vPos, Vec3 _vSize)
+{
+    CObject* pPlatform = CAbstractFactory<CPlatform>::Create(_vPos);
+    static_cast<CPlatform*>(pPlatform)->Set_PlatformSize(_vSize);
+    AddObject(BLOCK, pPlatform);
 }
